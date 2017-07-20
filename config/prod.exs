cb546d8a595c596d779ca4ddf140b00600e91ef4
Fernando Mendes <devfrmendes@gmail.com>
2017-07-20 12:40:32 +0100

    Disable SSL for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -16,8 +16,7 @@ use Mix.Config
 config :orwell, Orwell.Web.Endpoint,
   on_init: {Orwell.Web.Endpoint, :load_from_system_env, []},
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: System.get_env("HOST_URL"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: System.get_env("HOST_URL"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
